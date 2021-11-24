d49cbecc9386ecf559f6241a1a12bba7e74c4d83
Stefan Lelouda <stefanchromebook@gmail.com>
2021-11-24 12:32:48 +0100

    Efter mycket hjälp och hårdragande funkar det att lägga till och ta bort, har dock inte implementerat filtrering än

--- lib/SecondView.dart
+++ lib/SecondView.dart
@@ -1,39 +1,83 @@
 import 'package:flutter/material.dart';
+import './model.dart';
 
-class SecondView extends StatelessWidget {
+class SecondView extends StatefulWidget {
+  final TodoBox checkbox;
+
+  SecondView(this.checkbox);
+
   @override
+  State<StatefulWidget> createState() {
+    return _SecondViewState(checkbox);
+  }
+}
+
+class _SecondViewState extends State<SecondView> {
+  //hade problem med null värden, late gör att den inte körs förens den är kallad vilket gör att den inte blir null
+  late String message;
+  late bool true_false;
+  late TextEditingController textEditingController;
+
+  _SecondViewState(TodoBox box) {
+    this.message = box.TodoText;
+    this.true_false = box.true_false;
+
+    textEditingController = TextEditingController();
+
+    textEditingController.addListener(() {
+      setState(() {
+        message = textEditingController.text;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    textEditingController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: _field(),
+      appBar: AppBar(
+        title: const Text(
+          "TIG169 TODO",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(hintText: 'Vad ska du göra'),
+              controller: textEditingController,
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                  left: 16,
+                  right:
+                      16), //den verkar inte göra sitt jobb längre men får det inte att funka utan den...
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                ),
+                child: const Text('Add'),
+                onPressed: () {
+                  Navigator.pop(
+                      context, TodoBox(textEditingController.text, false));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-Widget _field() {
-  return ListView(
-    children: [
-      Container(
-        margin: EdgeInsets.only(left: 16, right: 16),
-        child: TextField(
-          decoration: InputDecoration(hintText: 'Vad ska du göra'),
-        ),
-      ),
-      Container(height: 24), //flytta på addknappen
-      _plusbox(),
-    ],
-  );
-}
 
-Widget _plusbox() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center, //lägger till knappen i mitten
-    children: [
-      Icon(
-        Icons.add,
-        size: 12,
-      ),
-      Text('Add'),
-    ],
-  );
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import './SecondView.dart';
 import 'package:provider/provider.dart';
+import './model.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => (TodoList()),
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,7 +23,10 @@ class MyApp extends StatelessWidget {
 
 class MainView extends StatefulWidget {
   //göra om till en statefulwidget
-  const MainView({Key? key}) : super(key: key); // vet ej vad detta är
+  const MainView({Key? key})
+      : super(
+            key:
+                key); // vet ej vad detta är, vet att const innebär att vi vet värdet och att det inte ändras
   @override
   _MainViewState createState() => _MainViewState();
 }
@@ -28,24 +39,52 @@ class _MainViewState extends State<MainView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TIG169 TODO'),
-      ),
-      body: ListView(
-        children: [],
-      ), //fluttade knappen till body istället samma som figma
+          title: const Text('TIG169 TODO'),
+          actions: []), //ska lägga till filtrering sen
+      body: Consumer<TodoList>(
+          builder: (context, state, child) => _field((state.list))),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add_task_rounded),
-          onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondView()),
-              )),
+        child: const Icon(Icons.add_task_rounded),
+        backgroundColor: Colors.blue,
+        onPressed: () async {
+          var newTodo = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SecondView(TodoBox('', false)),
+            ),
+          );
+          if (newTodo != null) {
+            Provider.of<TodoList>(context, listen: false).addTodoBox(newTodo);
+          }
+        },
+      ),
     );
   }
-}
 
-class Todobox {
-  String todo;
-  Todobox({
-    required this.todo,
-  });
+  Widget _field(title) {
+    return ListView.builder(
+        itemBuilder: (context, index) => _CheckBox(title[index]),
+        itemCount: title.length);
+  }
+
+  Widget _CheckBox(TodoBox checkbox) {
+    return CheckboxListTile(
+      value: checkbox.true_false,
+      activeColor: Colors.blue,
+      title: Text(
+        checkbox.TodoText,
+        style: TextStyle(
+          fontSize: 20,
+          decoration: checkbox.true_false ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      onChanged: (value) => setState(() => checkbox.true_false = value!),
+      secondary: IconButton(
+          icon: const Icon(Icons.delete),
+          onPressed: () async {
+            var state = Provider.of<TodoList>(context, listen: false);
+            state.DeleteTodoBox(checkbox);
+          }),
+    );
+  }
 }
